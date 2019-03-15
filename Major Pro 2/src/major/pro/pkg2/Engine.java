facfce4f6c1937fad96393fa368899c47626e4d8
Judith Hernandez <jhernandez-campillo@aggies.ncat.edu>
2019-03-15 15:29:54 -0400

    Engine Class

---
+++ Major Pro 2/src/major/pro/pkg2/Engine.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package major.pro.pkg2;
+
+/**
+ *
+ * @author Judith
+ */
+public class Engine {
+    private String manufacturer;
+    private double horsePower;
+    private int cylinders;
+    
+    public Engine(){
+        manufacturer = "";
+        horsePower = 0.0;
+        cylinders = 0;
+    }
+    public Engine(String manu, double horse, int cyl){
+        manufacturer = manu;
+        horsePower = horse;
+        cylinders = cyl;
+    }
+
+    /**
+     * @return the manufacturer
+     */
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    /**
+     * @param manufacturer the manufacturer to set
+     */
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    /**
+     * @return the horsePower
+     */
+    public double getHorsePower() {
+        return horsePower;
+    }
+
+    /**
+     * @param horsePower the horsePower to set
+     */
+    public void setHorsePower(double horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    /**
+     * @return the cylinders
+     */
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    /**
+     * @param cylinders the cylinders to set
+     */
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+    
+}
