e53a284224d62c5a8d8f753ad85d5537bfdc968d
Judith Hernandez <jhernandez-campillo@aggies.ncat.edu>
2019-03-26 02:06:00 -0400

    GasPoweredMower class

---
+++ Major Pro 2/src/major/pro/pkg2/GasPoweredMower.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package major.pro.pkg2;
+
+/**
+ *
+ * @author Judith
+ */
+public class GasPoweredMower {
+
+    private Engine engine;
+    private boolean selfPropelled;
+
+    public GasPoweredMower() {
+        engine = null;
+        selfPropelled = false;
+    }
+
+    /**
+     * @return the engine
+     */
+    public Engine getEngine() {
+        return engine;
+    }
+
+    /**
+     * @param engine the engine to set
+     */
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    /**
+     * @return the selfPropelled
+     */
+    public boolean isSelfPropelled() {
+        return selfPropelled;
+    }
+
+    /**
+     * @param selfPropelled the selfPropelled to set
+     */
+    public void setSelfPropelled(boolean selfPropelled) {
+        this.selfPropelled = selfPropelled;
+    }
+    @Override
+    public String toString(){
+        String nl = System.lineSeparator();
+        return engine + nl + selfPropelled;
+    }
+}
