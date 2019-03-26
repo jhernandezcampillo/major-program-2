f7335c78d9aa8869f2967cb9af1add8fe065d676
Judith Hernandez <jhernandez-campillo@aggies.ncat.edu>
2019-03-26 01:13:38 -0400

    LawnTractor class

---
+++ Major Pro 2/src/major/pro/pkg2/LawnTractor.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package major.pro.pkg2;
+
+/**
+ *
+ * @author Judith
+ */
+public class LawnTractor {
+    private Engine engine;
+    private String model;
+    private double deckWidth;
+    
+    public LawnTractor(){
+        engine = null;
+        model = "";
+        deckWidth = 0.0;
+    }
+
+    /**
+     * @return the engine
+     */
+    public Engine getEngine() {
+        return engine;
+    }
+
+    /**
+     * @param engine the engine to set
+     */
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the deckWidth
+     */
+    public double getDeckWidth() {
+        return deckWidth;
+    }
+
+    /**
+     * @param deckWidth the deckWidth to set
+     */
+    public void setDeckWidth(double deckWidth) {
+        this.deckWidth = deckWidth;
+    }
+    
+}
